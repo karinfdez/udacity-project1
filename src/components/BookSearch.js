c8b4baf8d687bdf6828a24d4c4352589dbe7fc99
karinfdez <karinfdez@yahoo.com>
2018-12-16 13:43:43 -0500

    Add link to homepage

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const BookSearch  = (props) => {
     return (
         <div className="search-books">
             <div className="search-books-bar">
-            <button className="close-search" 
-                onClick={() => 
-                    this.setState({ showSearchPage: false })}>
+            <Link to='/'>
+                <button className="close-search">
                     Close
-            </button>
+                </button>
+            </Link>
             <div className="search-books-input-wrapper">
                 {/*
                 NOTES: The search from BooksAPI is limited to a particular set of search terms.
