0d3f33eb7717f5f794d8444e1afd6fd1a4e39df0
karinfdez <karinfdez@yahoo.com>
2018-12-16 16:53:50 -0500

    change functional comp to class component

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,44 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import Book from './Book';
 
 
-const BookSearch  = (props) => {
-    
-    const searchBook = (e) => {
-        const query = e.target.value;
-        props.getAllBooksFromApi(query);
-
+class BookSearch  extends Component {
+    state = {
+        query: ''
     }
-    return (
-        <div className="search-books">
-            <div className="search-books-bar">
-            <Link to='/'>
-                <button className="close-search">
-                    Close
-                </button>
-            </Link>
-            <div className="search-books-input-wrapper">
-                <input 
-                    type="text" 
-                    onChange={searchBook} 
-                    placeholder="Search by title or author"
-                />
+    searchBook = (e) => {
+        this.setState({query: e.target.value}, () => {
+            this.props.getAllBooksFromApi(this.state.query);
+        });
+    }
+    render() {
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                <Link to='/'>
+                    <button className="close-search">
+                        Close
+                    </button>
+                </Link>
+                <div className="search-books-input-wrapper">
+                    <input 
+                        type="text" 
+                        onChange={this.searchBook} 
+                        placeholder="Search by title or author"
+                    />
+                </div>
+                </div>
+                <div className="search-books-results">
+                <ol className="books-grid">
+                    {this.props.books.length > 0 && this.props.books.map(book  => (
+                        <li key={book.id}>
+                            <Book 
+                                book={book} 
+                                changeShelf={this.props.changeShelf}
+                            />
+                        </li>
+                    ))}
+                </ol>
+                </div>
             </div>
-            </div>
-            <div className="search-books-results">
-            <ol className="books-grid">
-                {props.books.map(book  => (
-                    <li key={book.id}>
-                        <Book 
-                            book={book} 
-                        />
-                    </li>
-                ))}
-            </ol>
-            </div>
-        </div>
-    )
+        )
+    }
+  
 }
 
 export default BookSearch;
